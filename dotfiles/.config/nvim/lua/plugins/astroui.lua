c0197a9527343147877e73dacd1487f13b58736c
syrup3663ve <syrup3663ve@gmail.com>
2025-11-10 19:08:35 +0900

    update

--- dotfiles/.config/nvim/lua/plugins/astroui.lua
+++ dotfiles/.config/nvim/lua/plugins/astroui.lua
@@ -11,7 +11,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "astromars",
+    colorscheme = "tender",
     -- colorscheme = "astrodark",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
---
+++ dotfiles/.config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,6 @@
+return {
+  { "NLKNguyen/papercolor-theme" },
+  { "koron/vim-monochromenote" },
+  { "jacoborus/tender.vim" },
+  { "sample-usr/rakis.nvim" },
+}
